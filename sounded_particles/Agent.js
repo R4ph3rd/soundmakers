28ef23d00cfd4a0670470829d317e0db57b18db1
Raphaël Perraud <Hav4na@protonmail.com>
2019-04-28 23:16:51 +0200

    Add files via upload

--- sounded_particles/Agent.js
+++ sounded_particles/Agent.js
@@ -9,8 +9,8 @@ function Agent() {
     this.pos = createVector(random(width), random(height));
     this.oldPos = createVector(this.pos.x, this.pos.y);
     stepSize = random(1, 8);
-   // let colorPos = let (map(this.pos.x * this.pos.y,0,width * height, 0, c.length));
-       let cc = int(map (cos(this.pos.x * this.pos.y), -1, 0.9, 0, 3))
+
+       let cc = int(map (cos(this.pos.x * this.pos.y), -1, 0.9, 0, 2))
       if (cc > 3) cc = 3;
     cAgent = c[cc];
 
@@ -23,17 +23,12 @@ function Agent() {
 
       //mouse attraction
       if (mouseIsPressed == true){
-          if (dist(this.pos.x,this.pos.y, mouse.x, mouse.y) < 200 ) {
+          if (dist(this.pos.x,this.pos.y, mouse.x, mouse.y) < 350 ) {
             
             let gravity = p5.Vector.sub(mouse, this.pos); //make vector pointing towards centralPoint
-            //let distance = p5.Vector.mag(gravity); //distance between particle and centralPoint
-           // let gravitation = (9.81 * this.mass) / (distance * distance * 1.2); // formule de gravite pour la force gravitionnelle
             gravity.normalize();
             gravity.mult(9.81);
             this.pos.add(p5.Vector.div(gravity))
-
-           //let note = notes[int(random(notes.length))]
-           //synthe.triggerAttackRelease(note, '16n')
           }
 
       }
@@ -46,15 +41,11 @@ function Agent() {
         this.pos.y = random(height);
         this.oldPos.set(this.pos);
       }
-     // noisy += noisySpeed;
     }
 
     this.display = function(){
-     // noFill()
-      stroke(cAgent);
+      stroke(cAgent, 10);
       line(this.oldPos.x, this.oldPos.y, this.pos.x, this.pos.y);
-      //point(this.oldPos.x, this.oldPos.y)
-      //rect(this.pos.x, this.pos.y,1,1)
       this.oldPos.set(this.pos);
     }
 
